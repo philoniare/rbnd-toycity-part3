a7cb89eb5c59d8a3f4713c9288ffcba4a9f345f4
philoniare <philoniare@gmail.com>
2016-01-23 10:50:15 -0500

    Added methods for checking product stock

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,41 @@
 class Product
+    attr_reader :title, :price, :stock
     @@products = []
     def initialize(options={})
-        @@products << self
+        @title = options[:title]
+        @price = options[:price]
+        @stock = options[:stock]
+        add_to_products
     end
     def self.all
         @@products
+    end
+    def add_to_products
+        # check duplicate product
+        for product in @@products
+            if (product.title == @title)
+                raise DuplicateProductError, "#{@title} already exists."
+            end
+        end
+        @@products << self
+    end
+    def self.find_by_title(search_title)
+        for product in @@products
+            if (search_title == product.title)
+                return product
+            end
+        end
+    end
+    def in_stock?
+        @stock > 0
+    end
+    def self.in_stock
+        products_in_stock = []
+        for product in @@products
+            if (product.in_stock?)
+                products_in_stock << product
+            end
+        end
+        products_in_stock
     end
 end
